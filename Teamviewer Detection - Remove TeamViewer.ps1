619b1a02911b9846827656f704686f7c2c117122
Blackduke77 <Blackduke77@users.noreply.github.com>
2024-03-24 19:44:42 +0000

    Add files via upload

---
+++ Teamviewer Detection - Remove TeamViewer.ps1
@@ -0,0 +1,32 @@
+﻿# Function to check if TeamViewer or TeamViewer Host is installed
+function IsTeamViewerInstalled() {
+    try {
+        $z = 'TeamViewer'
+        
+        foreach ($h in 'HKLM') {
+            foreach ($x in 'SOFTWARE', 'SOFTWARE\wow6432node') {
+                $installedSoftware = Get-ItemProperty -Path "${h}:\$x\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like "*$z*" }
+
+                if ($installedSoftware) {
+                    Write-Output "TeamViewer or TeamViewer Host is installed."
+                    return $true
+                }
+            }
+        }
+
+        Write-Output "TeamViewer or TeamViewer Host is not installed."
+        return $false
+    } catch {
+        Write-Output "Error occurred: $_"
+        return $false
+    }
+}
+
+# Check if TeamViewer or TeamViewer Host is installed
+if (IsTeamViewerInstalled) {
+    Write-Output "TeamViewer or TeamViewer Host detected."
+    exit 1  # Detected
+} else {
+    Write-Output "TeamViewer or TeamViewer Host not detected."
+    exit 0  # Not Detected
+}
---
+++ Teamviewer Remediation - Remove TeamViewer.ps1
@@ -0,0 +1,15 @@
+﻿# Function to uninstall TeamViewer Host
+function UninstallTeamViewer {
+    $uninstallCommands = @(Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "*TeamViewer*" } | ForEach-Object { "msiexec.exe /x $($_.IdentifyingNumber) /qn /norestart" })
+    
+    foreach ($command in $uninstallCommands) {
+        Start-Process -FilePath "cmd.exe" -ArgumentList "/c $command" -WindowStyle Hidden -Wait
+    }
+}
+
+# Uninstall TeamViewer Host
+Write-Output "Uninstalling TeamViewer ..."
+
+UninstallTeamViewer
+
+Write-Output "TeamViewer  has been uninstalled."
